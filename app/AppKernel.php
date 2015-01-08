716c4a6301512c798463766b6e8af3b4c8fc8788
Wojciech Karski <wkarski@gmail.com>
2015-01-08 17:07:47 +0100

    cms bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Custom\CMSBundle\CustomCMSBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
